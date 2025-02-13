c95fbb3f25eca9e27d8bf94dec878bb7cfab2a59
Amit <heroku467@gmail.com>
2025-02-13 09:04:09 +0530

    Update and rename teletips_set (1).py to teletips_set.py

--- teletips_set.py
+++ teletips_set.py
@@ -26,13 +26,56 @@ bot = Client(
 )
 
 stoptimer = False
-
 TELETIPS_MAIN_MENU_BUTTONS = [
-    [
-        InlineKeyboardButton("👨‍💻 DEVELOPER", url="https://t.me/axa_bachha"),
-        InlineKeyboardButton("HELP ❓", callback_data="HELP_CALLBACK")
-    ]
-]
+            [
+                InlineKeyboardButton('👨‍💻 DEVELOPER', url='https://t.me/axa_bachha'),
+                InlineKeyboardButton('HELP ❓', callback_data="HELP_CALLBACK")
+            ]
+        ]
+
+@bot.on_message(filters.command(['start','help']) & filters.private)
+async def start(client, message):
+    text = START_TEXT
+    reply_markup = InlineKeyboardMarkup(TELETIPS_MAIN_MENU_BUTTONS)
+    await message.reply(
+        text=text,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True
+    )
+
+@bot.on_callback_query()
+async def callback_query(client: Client, query: CallbackQuery):
+    if query.data=="HELP_CALLBACK":
+        TELETIPS_HELP_BUTTONS = [
+            [
+                InlineKeyboardButton("⬅️ BACK", callback_data="START_CALLBACK")
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(TELETIPS_HELP_BUTTONS)
+        try:
+            await query.edit_message_text(
+                HELP_TEXT,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
+          
+    elif query.data=="START_CALLBACK":
+        TELETIPS_START_BUTTONS = [
+            [
+                InlineKeyboardButton('👨‍💻 DEVELOPER', url='https://t.me/axa_bachha'),
+                InlineKeyboardButton('HELP ❓', callback_data="HELP_CALLBACK")
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(TELETIPS_START_BUTTONS)
+        try:
+            await query.edit_message_text(
+                START_TEXT,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass    
+
 
 async def countdown_task(chat_id, msg_id, end_time, event_text):
     global stoptimer
