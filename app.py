825d0a51b87101d406d19e1f9c6b60ec3cd60654
Amit <heroku467@gmail.com>
2025-02-13 08:57:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,93 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run()
+
+
+
+stoptimer = False
+
+TELETIPS_MAIN_MENU_BUTTONS = [
+            [
+                InlineKeyboardButton('👨‍💻 DEVELOPER', url='https://t.me/axa_bachha'),
+                InlineKeyboardButton('HELP ❓', callback_data="HELP_CALLBACK")
+            ]
+        ]
+
+@bot.on_message(filters.command(['start','help']) & filters.private)
+async def start(client, message):
+    text = START_TEXT
+    reply_markup = InlineKeyboardMarkup(TELETIPS_MAIN_MENU_BUTTONS)
+    await message.reply(
+        text=text,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True
+    )
+
+@bot.on_callback_query()
+async def callback_query(client: Client, query: CallbackQuery):
+    if query.data=="HELP_CALLBACK":
+        TELETIPS_HELP_BUTTONS = [
+            [
+                InlineKeyboardButton("⬅️ BACK", callback_data="START_CALLBACK")
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(TELETIPS_HELP_BUTTONS)
+        try:
+            await query.edit_message_text(
+                HELP_TEXT,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
+
+    elif query.data=="GROUP_CALLBACK":
+        TELETIPS_GROUP_BUTTONS = [
+            [
+                InlineKeyboardButton("TeLe TiPs Chat [EN]", url="https://t.me/teletipsofficialontopicchat")
+            ],
+            [
+                InlineKeyboardButton("⬅️ BACK", callback_data="START_CALLBACK"),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(TELETIPS_GROUP_BUTTONS)
+        try:
+            await query.edit_message_text(
+                GROUP_TEXT,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass    
+
+    elif query.data=="TUTORIAL_CALLBACK":
+        TELETIPS_TUTORIAL_BUTTONS = [
+            [
+                InlineKeyboardButton("🎥 Video", url="https://youtu.be/nYSrgdIYdTw")
+            ],
+            [
+                InlineKeyboardButton("⬅️ BACK", callback_data="START_CALLBACK"),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(TELETIPS_TUTORIAL_BUTTONS)
+        try:
+            await query.edit_message_text(
+                TUTORIAL_TEXT,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass      
+          
+    elif query.data=="START_CALLBACK":
+        TELETIPS_START_BUTTONS = [
+            [
+                InlineKeyboardButton('👨‍💻 DEVELOPER', url='https://t.me/axa_bachha'),
+                InlineKeyboardButton('HELP ❓', callback_data="HELP_CALLBACK")
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(TELETIPS_START_BUTTONS)
+        try:
+            await query.edit_message_text(
+                START_TEXT,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass    
